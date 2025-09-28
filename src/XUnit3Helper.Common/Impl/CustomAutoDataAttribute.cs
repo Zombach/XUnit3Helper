94e933eca302aa7ee94a16ba497c3d74b6d64c75
ZLoo <Kozlov.s.v.1992@gmail.com>
2025-09-28 04:39:10 +0300

    upd job release/ fix propertires csproj

--- src/XUnit3Helper.Common/Impl/CustomAutoDataAttribute.cs
+++ src/XUnit3Helper.Common/Impl/CustomAutoDataAttribute.cs
@@ -7,19 +7,11 @@ namespace XUnit3Helper.Common.Impl;
 public class CustomAutoDataAttribute(Func<IFixture> fixtureFactory)
     : AutoDataAttribute(() => CreateFixture(fixtureFactory()))
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public CustomAutoDataAttribute()
         : this(() => CreateFixture(new Fixture()))
     {
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="fixture"></param>
-    /// <returns></returns>
     protected static IFixture CreateFixture(IFixture fixture)
     {
         fixture.Customize(new CompositeCustomization(new AutoMoqCustomization { ConfigureMembers = true }));
